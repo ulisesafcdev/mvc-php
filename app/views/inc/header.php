761ff46e842f479644b54e06890b5d0c88170577
ulisesafcdev <ulisesafc2020@gmail.com>
2022-11-10 12:11:18 -0600

    Cabezera HTML

---
+++ app/views/inc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?= URL . '/css/bootstrap.min.css' ?>">
+  <title><?= $datos['title'] ?></title>
+</head>
+<body>
