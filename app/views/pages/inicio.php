76ba80af3555437d66c5a5d9cc539117ffbcadd0
ulisesafcdev <ulisesafc2020@gmail.com>
2022-11-10 12:12:56 -0600

    Paginas del proyecto (vistas) que son cargadas desde el controlador

---
+++ app/views/pages/inicio.php
@@ -0,0 +1,9 @@
+<?php require_once APP . '/views/inc/header.php' ?>
+
+<div class="container-fluid bg-light py-5">
+  <div class="container">
+    <h1 class="display-4">Welcome!!!</h1>
+  </div>
+</div>
+
+<?php require_once APP . '/views/inc/footer.php' ?>
